c28416c28b07218d8b47445f489f866b5b3dd660
daodao1991 <471229015@qq.com>
2018-09-05 16:04:46 +0800

    构建DecisionTreeClassifier分类器

--- DecisionTree/DTree_ALLElectronics.py
+++ DecisionTree/DTree_ALLElectronics.py
@@ -3,6 +3,8 @@
 
 from sklearn.feature_extraction import DictVectorizer
 import csv
+from sklearn import preprocessing
+from sklearn import tree
 
 
 '''
@@ -11,11 +13,11 @@ import csv
 '''
 #首先使用DictVectorizer进行特征向量化
 allElectronicsData = open(r'E:\Python_jobs\MachineLearning\DecisionTree\AllElectronics.csv','r')
-reader = csv.reader(allElectronicsData)  #reader()会按行读取这个文件对象
-#header = reader.next()  此为Python2中的用法
+reader = csv.reader(allElectronicsData)#reader()会按行读取这个文件对象
+#header = reader.next()，为Python2中的用法
 #Python3中的用法如下
-headers = next(reader)  #此时headers即为第一行内容的一个列表，也就是把特征值的title存入headers这个变量中，以方便
-                        #接下来创建特征值的字典使用
+headers = next(reader)                 #此时headers即为第一行内容的一个列表，也就是把特征值的title存入headers这个
+                                       #变量中，以方便接下来创建特征值的字典使用
 
 
 print(reader)
@@ -32,12 +34,23 @@ for row in reader:
         rowDict[headers[i]] = row[i]   #！！！为每一行的字典添加键值对
     featureList.append(rowDict)
     
-print(featureList)  #此时featureList列表中的元素为，每一行特征值对应的字典
+print(featureList)                     #此时featureList列表中的元素为，每一行特征值对应的字典
 
 # 向量化特征列表
 vec = DictVectorizer()                 #注意DictVectorizer()的用法
-dummyX = vec.fit_transform(featureList).toarray()
+dummyX = vec.fit_transform(featureList).toarray()  #type(dummyX) --><class 'numpy.ndarray'>
 
 print("dummyX: " + '\n' + str(dummyX))
 print(vec.get_feature_names())
 
+# 向量化标记列表
+lb  = preprocessing.LabelBinarizer()
+dummyY = lb.fit_transform(labelList)   #type(dummyY) --><class 'numpy.ndarray'>
+
+print('dummyY: ' + '\n' + str(dummyY))
+
+# 使用DecisionTreeClassifier()
+clf = tree.DecisionTreeClassifier(criterion='entropy') #默认采用CART算法，这里设置标准为信息熵，即采用ID3算法
+clf = clf.fit(dummyX, dummyY)          #type(clf) --><class 'sklearn.tree.tree.DecisionTreeClassifier'>
+
+print('clf: ' + '\n' + str(clf))
